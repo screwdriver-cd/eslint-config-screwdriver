2682dcfa8075da2d5a25401ca22b229caecc5924
Jithin Emmanuel <jithin1987@gmail.com>
2020-03-10 15:21:33 -0700

    feat: upgrade to eslint6 (#40)
    BREAKING CHANGE: needs all client libraries to manually update and fix breaking rules.
    pull in new version of base airbnb rule
    Rule updates:
       remove conflictting indent rule
       increase line length to 120
       prefer-object-spread to warn
       relax restricted rules

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ module.exports = {
                 peerDependencies: false
             }
         ],
-        indent: ['error', 4],
-        'max-len': ['error', { code: 100, ignoreComments: true }],
+        'max-len': ['error', { code: 120, ignoreComments: true }],
         'max-lines-per-function': 'warn',
         'max-params': 'warn',
         'max-statements': 'warn',
@@ -39,12 +38,30 @@ module.exports = {
         ],
         'newline-after-var': ['error', 'always'],
         'newline-before-return': 'error',
+        'no-await-in-loop': 'warn',
         'no-bitwise': 'error',
         'no-multiple-empty-lines': ['error', { max: 1 }],
         'no-param-reassign': ['error', { props: false }],
+        // Override base rule to remove 'ForOfStatement'
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForInStatement',
+                message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
         'prefer-destructuring': 'warn',
         'prefer-rest-params': 'off',
         'prefer-spread': 'off',
+        'prefer-object-spread': 'warn',
         'prettier/prettier': [
             'error',
             {
